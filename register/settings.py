0e315f970e4fc791544d96b740d9d9922daa87cd
Aditya <ak.adityakumar421@gmail.com>
2024-08-18 14:26:38 +0530

    remove django version

--- register/settings.py
+++ register/settings.py
@@ -122,7 +122,22 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
 
 
+AWS_ACCESS_KEY_ID =  config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY =  config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME =  config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_SIGNATURE_NAME = config('AWS_S3_SIGNATURE_NAME')
+AWS_S3_REGION_NAME = 'eu-north-1'  
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACT = None
+AWS_S3_VERIFY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+
+# Static and media file configuration
+# STATIC_URL = f'{AWS_S3_URL_PROTOCOL}://{AWS_S3_CUSTOM_DOMAIN}/static/'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# MEDIA_URL = f'{AWS_S3_URL_PROTOCOL}://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 #Security:
 # 1.Cross-site Scripting (XSS):
