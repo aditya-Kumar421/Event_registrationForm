bfc3f008eaeec8af9c5338586e1c24d77c9967c9
Aditya <ak.adityakumar421@gmail.com>
2024-09-16 23:36:10 +0530

    add security

--- form/views.py
+++ form/views.py
@@ -65,7 +65,7 @@ class RegistrationList(APIView):
             mydict = {'name': name_value}    
             html_template = 'register_email.html'
             html_message = render_to_string(html_template, context = mydict)
-            subject = 'confirmation mail regarding event registration'
+            subject = "SPOCC'24 Registration Confirmation 🚀 Event Details Inside!"
             email_from = settings.EMAIL_HOST_USER
             email_data = serializer.validated_data.get('email')
             recipient_list = [email_data]
--- register/settings.py
+++ register/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["vercel.app", "onrender.com"]
 
 
 INSTALLED_APPS = [
@@ -38,7 +38,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ["https://registrationportal-hrxz.onrender.com", "https://registrationform308.vercel.app", "https://cccakgec.live"]
 ROOT_URLCONF = 'register.urls'
 
 TEMPLATES = [
@@ -91,10 +92,11 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.UserRateThrottle',
     ),
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '10/day',  
-        'user': '50/day',  
-    }
+        'anon': '3/day',  
+        'user': '7/day', 
+    },
 }
+
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
@@ -121,16 +123,16 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
 
 #Security:
 # 1.Cross-site Scripting (XSS):
-# SECURE_BROWSER_XSS_FILTER = True
-# SECURE_CONTENT_TYPE_NOSNIFF = True
-# CSRF_COOKIE_SAMESITE = 'strict'
-# SESSION_COOKIE_SAMESITE = 'strict'
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+CSRF_COOKIE_SAMESITE = 'strict'
+SESSION_COOKIE_SAMESITE = 'strict'
 
 #For production set below both lines to True
-# CSRF_COOKIE_HTTPONLY= True
-# SESSION_COOKIE_HTTPONLY= True
-# SECURE_SSL_REDIRECT = True
-# SECURE_HSTS_SECONDS = 31536000
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-# CSRF_COOKIE_SECURE = True
-# SESSION_COOKIE_SECURE = True  
+CSRF_COOKIE_HTTPONLY= True
+SESSION_COOKIE_HTTPONLY= True
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True  
