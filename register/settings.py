c5d7541a907b5bc8b58e35d890e506f73c228856
Aditya <ak.adityakumar421@gmail.com>
2024-09-18 23:12:07 +0530

    remove cors

--- register/settings.py
+++ register/settings.py
@@ -42,8 +42,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8000","https://registrationportal-hrxz.onrender.com", "https://registrationform308.vercel.app", "https://cccakgec.live"]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8000","https://registrationportal-hrxz.onrender.com", "https://registrationform308.vercel.app", "https://cccakgec.live"]
 ROOT_URLCONF = 'register.urls'
 
 TEMPLATES = [
