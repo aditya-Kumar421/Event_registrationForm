b4349db3bc6546811806abf375e38f4536b73e63
Aditya <ak.adityakumar421@gmail.com>
2024-09-18 23:40:24 +0530

    add cors

--- register/settings.py
+++ register/settings.py
@@ -10,12 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = [ "localhost", "127.0.0.1", 
-    'registrationform308.vercel.app', 
-    'registrationportal-hrxz.onrender.com',
-    'cccakgec.live'
-]
-
+# ALLOWED_HOSTS = [ "localhost", "127.0.0.1", 
+#     'registrationform308.vercel.app', 
+#     'registrationportal-hrxz.onrender.com',
+#     'cccakgec.live'
+# ]
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
